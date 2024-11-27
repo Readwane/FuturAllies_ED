4b11eaac29262a9313c174b18e796fc49b6e2697
Readwane <tegawendego@gmail.com>
2024-11-27 13:49:07 +0000

    mauvais commit à merge avec l'etat precedent pour remettre à un bon etat

--- frontend/src/app/features/admin/components/admin/admin.component.ts
+++ frontend/src/app/features/admin/components/admin/admin.component.ts
@@ -11,7 +11,7 @@ export class AdminComponent {
   @ViewChild('sidenav') sidenav: any;
   tooltips: any[] = [];  // Liste des tooltips à gérer
 
-  user = {
+  userConnectedd = {
     isLoggedIn: true,
     avatarUrl: '',
     name: 'Tegawende',
@@ -20,67 +20,6 @@ export class AdminComponent {
   };
 
   constructor(private overlayContainer: OverlayContainer, private el: ElementRef) {}
-
-
-  menuItems = [
-    { label: 'Tableau de bord', icon: 'dashboard', route: 'dashboard' },
-    {
-      label: 'Utilisateurs',
-      icon: 'people',
-      children: [
-        { label: 'Étudiants', route: 'users/students' },
-        { label: 'Formateurs', route: 'users/trainers' },
-        { label: 'Entreprises', route: 'users/enterprises' },
-        { label: 'Administrateurs', route: 'users/admins' },
-      ]
-
-    },
-    {
-      label: 'Écoles',
-      icon: 'school',
-      children: [
-        { label: 'Universités', route: '/universities' },
-        { label: 'Instituts', route: '/institutes' },
-        { label: 'Centres de formations', route: '/training-centers' }
-      ]
-    },
-    {
-      label: 'Cours',
-      icon: 'menu_book',
-      children: [
-        { label: 'Domaines', route: '/domains' },
-        { label: 'Parcours de formations', route: '/training-paths' },
-        { label: 'Cours libres', route: '/free-courses' },
-        { label: 'Cours payants', route: '/paid-courses' }
-      ]
-    },
-    {
-      label: 'Certifications',
-      icon: 'verified',
-      children: [
-        { label: 'Organismes de certifications', route: '/certification-bodies' },
-        { label: 'Certifications disponibles', route: '/available-certifications' },
-        { label: 'Certifications délivrées', route: '/issued-certifications' }
-      ]
-    },
-    {
-      label: 'Offres',
-      icon: 'work',
-      children: [
-        { label: 'Emplois', route: '/jobs' },
-        { label: 'Stages', route: '/internships' }
-      ]
-    },
-    {
-      label: 'Paramètres',
-      icon: 'settings',
-      children: [
-        { label: 'Thèmes', route: '/themes' },
-        { label: 'Apparence', route: '/appearance' },
-        { label: 'Politique d’utilisation', route: '/policy' }
-      ]
-    }
-  ];
 
   ngAfterViewInit(): void {
     // Vous pouvez ajouter ici des tooltips ou autres fonctionnalités interactives si nécessaire.
